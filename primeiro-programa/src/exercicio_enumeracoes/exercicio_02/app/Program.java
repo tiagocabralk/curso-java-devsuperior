503aeb3b760d6fdab0a73aebfa576a6716df672e
tiagocabralk <tiagocabralk@outlook.com>
2022-09-26 17:19:25 -0300

    Adicionado exercício novo sobre composição

---
+++ primeiro-programa/src/exercicio_enumeracoes/exercicio_02/app/Program.java
@@ -0,0 +1,33 @@
+package exercicio_enumeracoes.exercicio_02.app;
+
+import exercicio_enumeracoes.exercicio_02.entities.Comment;
+import exercicio_enumeracoes.exercicio_02.entities.Post;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Comment c01 = new Comment("Have a nice trip");
+        Comment c02 = new Comment("Wow that's awesome!");
+        Comment c03 = new Comment("Good night!");
+        Comment c04 = new Comment("May the Force br with you");
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyy HH:mm:ss");
+        LocalDateTime momento = LocalDateTime.now();
+
+        Post post01 = new Post(momento, "Traveling to New Zelland", "I'm going to visit this wonderful country!", 12);
+
+        Post post02 = new Post(momento, "Good night guys", "See you tomorrow", 5);
+
+        System.out.println(post01.getTitle());
+        System.out.println(post01.getLikes() + " Likes - " + post01.getMoment().format(formatter));
+        System.out.println(post01.getContent());
+        System.out.println("Comments:");
+
+        System.out.println(post01.addComment(c01));
+    }
+}
---
+++ primeiro-programa/src/exercicio_enumeracoes/exercicio_02/entities/Comment.java
@@ -0,0 +1,20 @@
+package exercicio_enumeracoes.exercicio_02.entities;
+
+public class Comment {
+    private String text;
+
+    public Comment() {
+    }
+
+    public Comment(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
